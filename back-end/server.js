68fd9ef0a2f59c85c49e2b47e30214837a9c9fa3
Alpha <jbcavelier@yahoo.fr>
2022-11-30 20:53:54 +0100

    structure backend

---
+++ back-end/server.js
@@ -0,0 +1,37 @@
+// import express module and create your express app
+const express = require('express');
+const app = express();
+
+// set the server host and port
+const port = 3000;
+
+// add data to req.body (for POST requests)
+app.use(express.urlencoded({ extended: true }));
+
+// serve static files
+app.use(express.static('../frontend'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// import  and use express-session module
+const session = require('express-session');
+app.use(session({
+	secret: 'login', //used to sign the session ID cookie
+	name: 'login', // (optional) name of the session cookie
+	resave: true, // forces the session to be saved back to the session store
+	saveUninitialized: true, // forces a session an uninitialized session to be saved to the store	
+}));
+
+// routers
+
+
+
+
+
+
+// run the server
+app.listen(port, () => {
+	// callback executed when the server is launched
+	console.log(`Express app listening on port ${port}`);
+});
