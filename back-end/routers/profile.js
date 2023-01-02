b45eb0f7f54b9be4494c8f57568f4c197cfb1040
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2023-01-02 16:19:09 +0100

    affichage du pseudo et compression des template

--- back-end/routers/profile.js
+++ back-end/routers/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/', (req, res) => {
-    res.render('profile.ejs', { logged: req.session.loggedin, login: req.session.login });
+    res.render('profile.ejs', { logged: req.session.loggedin, login: req.session.login, testest:"ouioui"});
 });
 
 module.exports = router;
