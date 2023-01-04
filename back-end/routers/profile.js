d8b82bad47a673d15886f0d0f48cc147e9dca10b
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2023-01-04 14:21:37 +0100

    Si l'utilisateur n'est pas logged alors il redirigé vers login ajout du front pour les stats

--- back-end/routers/profile.js
+++ back-end/routers/profile.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const usersUtil = require('./usersUtilitaries')
 
 router.use('/', (req, res) => {
-    res.render('profile.ejs', { logged: req.session.loggedin, login: req.session.login, testest:"ouioui"});
+    /**
+     * @author Alexis IMBERT
+     *  */
+    if (!usersUtil.isLoggedIn(req)) { 
+        res.redirect("/users/login")
+        return;
+    } ;
+    res.render('profile.ejs', { logged: req.session.loggedin, login: req.session.login, couleur_pref:"couleur pref",nb_pixel_pose:"nb_pixel_pose",nb_canvas:"nb_canva",canvas_plus_actif:"canvas + actif ", nb_pixel_moyen:"nb_pixel_moyen"});
 });
 
 module.exports = router;
