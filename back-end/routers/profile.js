2cbed441941e8bb18218a55347ed0ded42e162ad
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2023-01-04 14:30:42 +0100

    Ajout de commentaire

--- back-end/routers/profile.js
+++ back-end/routers/profile.js
@@ -1,3 +1,4 @@
+/**  @author Alexis IMBERT */
 const express = require('express');
 const router = express.Router();
 const usersUtil = require('./usersUtilitaries')
@@ -6,11 +7,11 @@ router.use('/', (req, res) => {
     /**
      * @author Alexis IMBERT
      *  */
-    if (!usersUtil.isLoggedIn(req)) { 
+    if (!usersUtil.isLoggedIn(req)) {
         res.redirect("/users/login")
         return;
-    } ;
-    res.render('profile.ejs', { logged: req.session.loggedin, login: req.session.login, couleur_pref:"couleur pref",nb_pixel_pose:"nb_pixel_pose",nb_canvas:"nb_canva",canvas_plus_actif:"canvas + actif ", nb_pixel_moyen:"nb_pixel_moyen"});
+    };
+    res.render('profile.ejs', { logged: req.session.loggedin, login: req.session.login, couleur_pref: "couleur pref", nb_pixel_pose: "nb_pixel_pose", nb_canvas: "nb_canva", canvas_plus_actif: "canvas + actif ", nb_pixel_moyen: "nb_pixel_moyen" });
 });
 
 module.exports = router;
